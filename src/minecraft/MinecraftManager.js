8985db39f3778d67066e76a9b9cf3a244476d5f1
Tincancrafter <chrisreyesmailbox@gmail.com>
2026-02-17 14:57:45 -0500

    Update MinecraftManager.js

--- src/minecraft/MinecraftManager.js
+++ src/minecraft/MinecraftManager.js
@@ -30,7 +30,7 @@ class MinecraftManager extends CommunicationBridge {
       port: this.app.config.server.port,
       username: this.app.config.minecraft.username,
       password: this.app.config.minecraft.password,
-      version: "1.21.10",
+      version: "1.21.11",
       auth: this.app.config.minecraft.accountType,
     })
   }
